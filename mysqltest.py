1e2152906068025094f08ef5959c4df34d4f8e90
Miffy <maiwenchao@pinquest.cn>
2019-06-04 12:52:35 +0800

    mysql conf

--- mysqltest.py
+++ mysqltest.py
@@ -1,17 +1,17 @@
 import pymysql
 
 config = {
-    'host': '192.168.10.10',
+    'host': '47.103.20.2',
     'port': 3306,
-    'user': 'homestead',
-    'passwd': 'secret',
+    'user': 'riku',
+    'passwd': 'riku0806',
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
 conn = pymysql.connect(**config)
 conn.autocommit(1)
 cursor = conn.cursor()
-conn.select_db('miffy')
+conn.select_db('riku')
 
 cursor.execute("SELECT * FROM products")
 for r in cursor:
