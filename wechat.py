4ca50b81103b1bc327633b8f3161ee92ff51eeba
Miffy <maiwenchao@pinquest.cn>
2019-06-19 21:58:43 +0800

    wechat ok

--- wechat.py
+++ wechat.py
@@ -1,10 +1,8 @@
 import itchat
 import threading
 import logging
-import time
 import db
 import datetime
-import pytz
 
 interval = 1  # 频率(间隔多少分钟)
 logger = logging.getLogger('wechat')
@@ -14,30 +12,31 @@ logger.addHandler(fh)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 
-# itchat.auto_login()
+itchat.auto_login()
 
 
 def worker():
     logger.info('working...')
-    # author = itchat.search_chatrooms(name='琪琪快乐买货宝')[0]
+    author = itchat.search_chatrooms(name='琪琪快乐买货宝')[0]
 
     # 拉数据
-    tz = pytz.timezone("Asia/Shanghai")
-    now = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
-    print(now)
-
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # print("SELECT * FROM products WHERE last_stock_time >= DATE_ADD('%s', INTERVAL '-%s' MINUTE)" % (now, interval))
+    db.cursor.execute("SELECT * FROM products WHERE last_stock_time >= DATE_ADD('%s', INTERVAL '-%s' MINUTE)" % (now, interval))
+    if db.cursor.rowcount > 0:
+        ps = db.cursor.fetchall()
+        msg = ''
+        for p in ps:
+            m = p['shop_name']+'的'+p['spu_name']+' ['+p['sku_name']+'] '+'有货喇!!!'+'\n'
+            msg += m
 
-    # db.cursor.execute("SELECT * FROM products WHERE last_stock_time >= DATE_ADD('%s', INTERVAL '-%s' MINUTE)" % (now, interval))
-    # if db.cursor.rowcount > 0:
-    #     products = db.cursor.fetchall()
-    # print(products)
+        author.send(msg)
 
-    # msg = '报时信号最后一响...现在是标准时间'+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    # author.send(msg)
-    # global timer
-    # timer = threading.Timer(60*interval, worker)
-    # timer.start()
+    logger.info('nothing happen...')
+
+    global timer
+    timer = threading.Timer(60*interval, worker)
+    timer.start()
 
 
 worker()
